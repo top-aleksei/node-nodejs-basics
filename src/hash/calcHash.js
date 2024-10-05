ae1caee5f2465d946e211cf2a33fbd7c824f38e9
Aleksei <a.toparau@andersenlab.com>
2024-10-05 16:21:40 +0300

    feat: modules, hash, stream solutions

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import path from 'node:path';
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const { dirname } = import.meta;
+  const fileToHashPath = path.join(dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+  const readStream = createReadStream(fileToHashPath);
+
+  readStream.on('data', (data) => {
+    hash.update(data);
+  });
+
+  readStream.on('end', () => {
+    console.log(hash.digest('hex'));
+  });
 };
 
 await calculateHash();
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+  const { dirname } = import.meta;
+  const filePath = path.join(dirname, 'files', 'fileToRead.txt');
+  const readStream = createReadStream(filePath, { encoding: 'utf-8' });
+
+  readStream.on('data', (data) => {
+    stdout.write(data);
+  });
+  readStream.on('end', () => {
+    console.log('\n');
+  });
 };
 
 await read();
