25f9821e935160cf6537017ebcf81175960a581c
Aleksei <a.toparau@andersenlab.com>
2024-10-05 16:43:05 +0300

    feat: resolve wt and zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import path from 'node:path';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const { dirname } = import.meta;
+  const fromPath = path.join(dirname, 'files', 'fileToCompress.txt');
+  const toPath = path.join(dirname, 'files', 'archive.gz');
+
+  const input = createReadStream(fromPath);
+  const output = createWriteStream(toPath);
+
+  const zip = createGzip();
+
+  input.pipe(zip).pipe(output);
 };
 
 await compress();
